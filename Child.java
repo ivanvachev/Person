7c90ae393dba1ae73febbc1ab46f3950d62a0a69
Ivan Vachev <Ivan Vachev>
2017-03-01 19:32:33 +0200

    New project

---
+++ Child.java
@@ -0,0 +1,18 @@
+package Person;
+
+public class Child extends Person{
+
+    public Child (String name, int age)  {
+        super(name, age);
+    }
+    @Override
+    protected void setAge (int age) throws IllegalArgumentException {
+        if (age > 15) {
+            throw  new IllegalArgumentException("Child’s age must be less than 15!");
+        }
+        else {
+            super.setAge(age);
+        }
+    }
+
+}
